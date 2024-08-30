c48148dd378d77bed5cd39d3b32fe7ff3fa7d2ea
Gonzalo Huerta Canepa <gonzalo@huerta.cl>
2024-08-29 20:29:26 -0400

    adding my first program

---
+++ user/first.c
@@ -0,0 +1,14 @@
+//A Simple C program 
+#include "kernel/types.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+	
+//passing command line arguments 
+	
+int main(int argc, char *argv[]) 
+{ 
+    printf("My first xv6 program learnt at GFG\n"); 
+    exit(0); 
+} 
+
+// This code is contributed by sambhav228 
